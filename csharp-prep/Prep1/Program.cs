084310f91f0507558f723e033d54a3d309310e88
randymz10 <randymz10@gmail.com>
2024-11-08 02:03:35 -0300

    Finished the first 2 exercises

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        String firstName = "";
+        String lastName = "";
+
+        Console.WriteLine("What is your first name?");
+        firstName = Console.ReadLine();
+
+        Console.WriteLine("What is your last name?");
+        lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        int percentage = 0;
+        String grade = "";
+
+        Console.WriteLine("What is your grade percentage?");
+        percentage = int.Parse(Console.ReadLine());
+
+        if (percentage >= 90)
+        {
+            grade = "A";
+        }
+        else if (percentage >= 80)
+        {
+            grade = "B";
+        }
+        else if (percentage >= 70)
+        {
+            grade = "C";
+        }
+        else if (percentage >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {grade}");
+
+        if (percentage > 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You need to make more effort");
+        }
     }
 }
