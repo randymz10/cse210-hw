cf8dec4f7a83daaadb64d74a8992178293449c96
randymz10 <randymz10@gmail.com>
2024-11-08 16:12:09 -0300

    finished exeercise 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int magicNumber = new Random().Next(1, 101);
+        int guess = 0;
+        String message = "";
+
+        while (magicNumber != guess)
+        {
+
+            Console.WriteLine("What is your guess?");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                message = "Higher";
+            }
+            else if (magicNumber < guess)
+            {
+                message = "Lower";
+            }
+            else
+            {
+                message = "You guessed it!";
+            }
+
+            Console.WriteLine(message);
+        }
     }
 }
