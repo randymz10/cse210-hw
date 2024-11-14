f5c4e2385ebb93f4d5b1be5f37c25f6c8f41084a
randymz10 <randymz10@gmail.com>
2024-11-14 00:27:56 -0300

    Finished learning 2 activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        Job job2 = new Job();
+
+        job1._jobTitle = "Software Enginer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+        
+        job2._company = "Apple";
+        job2._jobTitle = "Manager";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+        
+        job1.Display();
+        job2.Display();
+
+
+        Resume resume = new Resume();
+
+        resume._name = "Allison Rose";
+        
+        List<Job> jobs = new List<Job>();
+        jobs.Add(job1);
+        jobs.Add(job2);
+
+        resume._jobs = jobs;
+        
+        resume.Display();
     }
 }
